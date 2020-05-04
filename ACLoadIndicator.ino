295f4433034e3e8052632b51a32fdcccaf801263
grafi71 <52415763+grafi71@users.noreply.github.com>
2020-05-04 13:37:01 +0200

    Update ACLoadIndicator.ino

--- ACLoadIndicator.ino
+++ ACLoadIndicator.ino
@@ -5,11 +5,13 @@
 
 /* Number of AC reads to be performed to get min and max values */
 #define NO_ADC_READS 1700
+/* Number of LEDs in a bar */
+#define NO_LEDS 10
 
 /* Analog input pin to connect directly to sensor */
 const int analogInPin = A0;  
 /* Arduino outputs to be connected with LEDs */
-const int ledBarPins[] = {2,3,4,10,11,5,6,7,8,9};
+const int ledBarPins[NO_LEDS] = {2,3,4,10,11,5,6,7,8,9};
 /* Store value red from sensor */
 int sensorValue = 0;  
 /* Store min value */
@@ -22,7 +24,7 @@ int difference = 0;
 int scale = 80;
 
 void setup() {
-  for(int i=0; i<10; i++) {
+  for(int i=0; i<NO_LEDS; i++) {
     pinMode(ledBarPins[i], OUTPUT);
     digitalWrite(ledBarPins[i], HIGH);
   }
@@ -36,7 +38,7 @@ void loop() {
     if(sensorValue<minValue) { minValue = sensorValue; }
   }
   difference = maxValue - minValue;
-  for(int i=0; i<10; i++) {
+  for(int i=0; i<NO_LEDS; i++) {
     digitalWrite(ledBarPins[i], (difference>=scale*i)?LOW:HIGH);
   }
 }
