882e515053aa4178ce2a9ec822b7ff604b191a27
grafi71 <52415763+grafi71@users.noreply.github.com>
2020-05-04 12:45:24 +0200

    Update ACLoadIndicator.ino

--- ACLoadIndicator.ino
+++ ACLoadIndicator.ino
@@ -1,16 +1,24 @@
 /*
-  Power indicator, bar led is scaled for 20A sensor to show max load of 16A
+  Arduino AC Power indicator using LED bar with 10 LEDs
+  Configured for 20A sensor SC-013 type
 */
 
+/* Number of AC reads to be performed to get min and max values */
 #define NO_ADC_READS 1700
 
-const int analogInPin = A0;  // Analog input pin 
-const int ledBarPins[]={2,3,4,10,11,5,6,7,8,9};
-
-int sensorValue = 0;        // value read from the pot
+/* Analog input pin to connect directly to sensor */
+const int analogInPin = A0;  
+/* Arduino outputs to be connected with LEDs */
+const int ledBarPins[] = {2,3,4,10,11,5,6,7,8,9};
+/* Store value red from sensor */
+int sensorValue = 0;  
+/* Store min value */
 int minValue = 0;
+/* Store max value */
 int maxValue = 0;
+/* Calculate difference (maxValue-minValue) */
 int difference = 0;
+/* Linear values to be compared with calculated difference value to light appropriate number of LEDs */
 int scaled[] = {0, 80, 160, 240, 320, 400, 480, 560, 640, 720};
 
 void setup() {
